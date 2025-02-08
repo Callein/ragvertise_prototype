46c3878fb76273397ed730f3d726e2af3522644a
callendev20 <callendev20@gmail.com>
2025-02-08 16:26:41 +0900

    :white_check_mark: Add util package - database, date_tool, io_utils

---
+++ util/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+
+from constants.env_variables import EnvVariables
+
+load_dotenv()
+
+db_host = EnvVariables.DB_HOST
+db_name = EnvVariables.DB_NAME
+db_username = EnvVariables.DB_USERNAME
+db_password = EnvVariables.DB_PASSWORD
+db_port = EnvVariables.DB_PORT
+DATABASE_URL = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# 모든 모델의 부모
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
